25e9adc71fce21747fae887b229574d73bd02c60
maxibondi <167434823+maxibondi@users.noreply.github.com>
2025-02-14 12:21:59 -0300

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -3,7 +3,5 @@ import react from '@vitejs/plugin-react';
 
 export default defineConfig(({ command }) => ({
   plugins: [react()],
-  base: "./", // Asegura que los assets se carguen bien en producción
-  server: command === "serve" ? { host: "0.0.0.0" } : undefined
 }));
 
